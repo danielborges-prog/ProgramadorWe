507d9c1120c70d92359c2fa3c28899724fd7dec1
danielborges-prog <engmecanicodaniel@gmail.com>
2021-01-12 21:37:38 -0300

    commit de strings

---
+++ Aula110121/Aula120121/type.properties.js
@@ -0,0 +1,7 @@
+//Propriedades de Strings no JavaScript
+//Comprimento
+console.log('Este é o comprimento de uma string!'.length); //Comprimento
+console.log('Este é um  \bbackspace no JavaScript'); //Backspace
+console.log('ISTO É UMA STRING MAIÚSCULA PARA MINÚSCULA'.toLowerCase()); //Maiúscula para minúscula
+console.log('isto é uma string minúscula para maisúcula'.toUpperCase()); //Minúscula para maiúscula
+console.log('Organiza os espaços no início e fim do texto!'.);
---
+++ Aula110121/Aula120121/type.string.js
@@ -0,0 +1,25 @@
+/*
+//Tipos de dados usados pelo JS: strings
+console.log('Isso é uma String!');
+console.log("Isso é uma String!");
+console.log(`Usado também dentro de crase!`);
+
+console.log("It's seven o'clock"); //aceita
+// console.log('It's seven o'clock'); //não aceita
+
+//console.log('Essa não pode ser como 'string!'');
+console.log('Essa pode ser como "string"');
+//console.log('Essa não pode ser como 'string'!'');
+console.log("Essa não pode ser como 'string!'"); 
+*/
+
+//Como imprimir caracteres especiaias:
+/*console.log('Isso é um \'string!\'');
+console.log("Isso é um \"string!\"");
+console.log('\' '); //Imprimir aspa simples
+console.log("\" "); //Imprimir aspa dupla
+console.log("\\"); // Imprimir barra dupla
+*/
+console.log('Esta é uma \nquebra de linha no JavaScript!'); //Quebra de linha
+console.log('Este é um \rretorno de carro no JavaScript!'); //Retorno de carro
+console.log('Esta é uma \vtabulação normal no JavaScript!'); //Tabulação normal
